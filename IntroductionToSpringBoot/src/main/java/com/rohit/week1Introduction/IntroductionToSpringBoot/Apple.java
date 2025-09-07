b7044ad3f8f80c55d8bbe62e42f3dbda3c2281d7
Rohit Rathore <rohitjeevanrathore@gmail.com>
2025-09-07 12:30:35 +0530

    implemented @Component and @Autowired

--- IntroductionToSpringBoot/src/main/java/com/rohit/week1Introduction/IntroductionToSpringBoot/Apple.java
+++ IntroductionToSpringBoot/src/main/java/com/rohit/week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -1,5 +1,9 @@
 package com.rohit.week1Introduction.IntroductionToSpringBoot;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Apple {
     void eating(){
         System.out.println("I am eating an Apple");
